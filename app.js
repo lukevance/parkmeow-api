93f8d586b2ce11d1309387ce5ddf7e9da57b505d
lvance <lvance@twilio.com>
2021-12-09 16:02:03 -0500

    dynamically process number

--- app.js
+++ app.js
@@ -3,30 +3,40 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const serviceSid = process.env.VERIFY_SERVICE_SID;
 
+const client = require('twilio')(accountSid, authToken);
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-const client = require('twilio')(accountSid, authToken);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Let me park right meow!');
 });
 
 app.post('/send-code', (req, res) => {
-    client.verify.services(process.env.VERIFY_SERVICE_SID)
+    try {
+        if (req.body && req.body.phone) {
+            client.verify.services(serviceSid)
              .verifications
-             .create({to: process.env.MY_PHONE, channel: 'sms'})
+             .create({to: req.body.phone, channel: 'sms'})
              .then(verification => {
                  console.log(verification.status);
                  res.send(`you are: ${verification.status}`);
-             });
-    res.send();
-})
+             });    
+        } else {
+            throw 'No phone number provided!'
+        }
+    } catch (error) {
+        console.log(error);
+        res.send(`your number ${req.body.phone} is not valid. Please provide another.`);    
+    }
+});
 
 // verify the code the user provided, return a temp authtoken (store the token somewhere)
 app.post('/verify-code', (req, res) => {
-    client.verify.services(process.env.VERIFY_SERVICE_SID)
+    client.verify.services(serviceSid)
       .verificationChecks
       // TODO fix from twilio's sample
       .create({to: '+15017122661', code: '1234'})
