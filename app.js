049dbdb9c622bf333f66e51b9ff57810505097fe
lvance <lvance@twilio.com>
2021-12-09 22:14:00 -0500

    added airtable integration

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const serviceSid = process.env.VERIFY_SERVICE_SID;
+const airtableKey = process.env.AIRTABLE_API_KEY;
+const airtableBaseID = process.env.AIRTABLE_BASE_ID;
 
 const client = require('twilio')(accountSid, authToken);
+
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: airtableKey}).base(airtableBaseID);
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 app.get('/', (req, res) => {
   res.send('Let me park right meow!');
@@ -38,13 +49,59 @@ app.post('/send-code', (req, res) => {
 app.post('/verify-code', (req, res) => {
     client.verify.services(serviceSid)
       .verificationChecks
-      // TODO fix from twilio's sample
-      .create({to: '+15017122661', code: '1234'})
-      .then(verification_check => console.log(verification_check.status));
-})
+      .create({to: req.body.phone, code: req.body.code})
+      .then(verification_check => {
+          console.log(verification_check.status);
+          // TODO save user to db with token, return token with status
+          let poorAuthToken = getRandomInt(100000, 999999);
+          base('Users').create([
+            {
+              "fields": {
+                "number": req.body.phone,
+                "auth_token": poorAuthToken
+              }
+            }
+          ], (err, records) => {
+            if (err) {
+                console.error(err);
+                res.json(err);
+            } else {
+                console.log(records);
+                if (records.length > 0) {
+                    res.json(records[0].fields);
+                } else {
+                    res.json(records);
+                }
+            }
+            return;
+          });
+      });
+});
 
 // create a new parking session, validate via the temp authtoken
+app.post('/parking-session', (req, res) => {
+    if (!req.body.auth_token) res.status(401).send('No auth_token provided');
+    // check for auth_token before creating
+    base('Users').select({
+        view: 'Grid view',
+        filterByFormula: "{auth_token} = "  + req.body.auth_token
+    }).firstPage((err, records) => {
+        if (err) { 
+            console.error(err); 
+            res.send(err);
+            return;
+        } else if (records.length < 1) {
+            res.status(401).send('Bad auth_token');
+            return;
+        }
+        const items = records.map(rec => rec.fields);
+        res.json(items);
+    });
+
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
